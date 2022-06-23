0bcc7f502a3dfc0b478b54bf5c97be3510f023ff
dhkaneda <dennishuynh@protonmail.com>
2022-06-23 03:34:15 -0700

    Remove unecessary credentials

--- db/index.js
+++ db/index.js
@@ -1,9 +1,6 @@
-// Authenticate to AWS using our credentials, also on the Node env process
 const AWS = require('aws-sdk');
 AWS.config.update({
-  region: process.env.AWS_DEFAULT_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+  region: process.env.AWS_DEFAULT_REGION
 });
 
 // You'll need to call dynamoClient methods to invoke CRUD operations on the DynamoDB table
